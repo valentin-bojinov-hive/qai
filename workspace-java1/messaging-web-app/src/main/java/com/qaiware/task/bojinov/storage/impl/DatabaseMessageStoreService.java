1f224c4f328a5db9a2fcf7014d8af82241747c38
valio <valio@G771JW>
2016-11-07 05:41:23 +0200

    Fix get endpoint

--- workspace-java1/messaging-web-app/src/main/java/com/qaiware/task/bojinov/storage/impl/DatabaseMessageStoreService.java
+++ workspace-java1/messaging-web-app/src/main/java/com/qaiware/task/bojinov/storage/impl/DatabaseMessageStoreService.java
@@ -13,45 +13,59 @@ import org.springframework.stereotype.Service;
 
 import com.qaiware.task.bojinov.model.IMessage;
 import com.qaiware.task.bojinov.model.MessageTypesEnum;
+import com.qaiware.task.bojinov.model.impl.EmotionMessage;
+import com.qaiware.task.bojinov.model.impl.TextMessage;
 import com.qaiware.task.bojinov.storage.IMessageStoreService;
 import com.qaiware.task.bojinov.storage.entity.MessageEntity;
 import com.qaiware.task.bojinov.storage.exceptions.StorageFailureException;
+
 /**
  * Service for storing Messages to the database
+ * 
  * @author valio
  *
  */
 @Service
 public class DatabaseMessageStoreService implements IMessageStoreService {
-	
-	
-	
+
 	@Override
 	public void store(final IMessage message) throws StorageFailureException {
-		
+
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("DefaultPersistenceUnit");
 		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		entityManager.getTransaction().begin();
 		entityManager.persist(toMessageEntity(message));
 		entityManager.getTransaction().commit();
-		
+
 	}
 
 	@Override
 	public List<IMessage> getMessages() {
 		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("DefaultPersistenceUnit");
-		EntityManager entityManager = entityManagerFactory.createEntityManager();		
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
 		Query query = entityManager.createQuery("from MessageEntity");
-		return query.getResultList();
+		List<IMessage> result = new ArrayList<IMessage>(query.getResultList().size());
+		for (MessageEntity entity : (List<MessageEntity>) query.getResultList()) {
+			if (entity.getType().equals(MessageTypesEnum.EMOTION.type())) {
+				result.add(new EmotionMessage(entity.getPayload()));
+
+			}
+
+			if (entity.getType().equals(MessageTypesEnum.TEXT.type())) {
+				result.add(new TextMessage(entity.getPayload()));
+
+			}
+
+		}
+		return result;
 	}
 
 	@Override
 	public void clear() {
-		//XXX Used only for the Unit test implementation
+		// XXX Used only for the Unit test implementation
 	}
-	
-	private MessageEntity toMessageEntity(final IMessage message)
-	{
+
+	private MessageEntity toMessageEntity(final IMessage message) {
 		MessageEntity entity = new MessageEntity();
 		entity.setPayload(message.getMessage());
 		entity.setType(message.getType().type());
@@ -62,15 +76,12 @@ public class DatabaseMessageStoreService implements IMessageStoreService {
 	@Override
 	public List<IMessage> getMessages(MessageTypesEnum type) {
 		List<IMessage> filteredList = new ArrayList<IMessage>();
-		for (IMessage message : getMessages())
-		{
-			if (message.getType().equals(type))
-			{
+		for (IMessage message : getMessages()) {
+			if (message.getType().equals(type)) {
 				filteredList.add(message);
 			}
 		}
 		return filteredList;
 	}
-	
-	
+
 }
